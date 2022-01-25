7a66c96b911a27cf6a829ade2a998d66dad5cdbf
Raj Chauhan <world.raj95@gmail.com>
2022-01-25 14:16:34 +0530

    User BO Update

--- src/main/java/com/toqqa/bo/UserBO.java
+++ src/main/java/com/toqqa/bo/UserBO.java
@@ -1,5 +1,23 @@
 package com.toqqa.bo;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.Nullable;
+
 public class UserBO {
 
+	@Size(max = 1024)
+	@NotNull
+	@NotBlank
+	private String firstName;
+	
+	@Size(max = 1024)
+	@NotNull
+	@NotBlank
+	private String lastName;
+	
+	@Nullable
+	private String email;
 }
