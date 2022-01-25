5b9295031d67517194c1851da547c8f2e1fa3bcf
Raj Chauhan <world.raj95@gmail.com>
2022-01-25 12:21:17 +0530

    Initial Commit

---
+++ src/main/java/com/toqqa/ToqqaApplication.java
@@ -0,0 +1,13 @@
+package com.toqqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToqqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToqqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toqqa/ToqqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toqqa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToqqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
