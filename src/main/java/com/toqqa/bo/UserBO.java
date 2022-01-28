b0d33abfb06227e32cbcc83afada78fa3e655213
'Rajat <rajat.sharma@biztecno.net>
2022-01-28 15:03:11 +0530

    #TOQQA-2 initial bo and domain

--- src/main/java/com/toqqa/bo/UserBO.java
+++ src/main/java/com/toqqa/bo/UserBO.java
@@ -1,23 +1,53 @@
 package com.toqqa.bo;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import com.toqqa.domain.User;
 
-import org.springframework.lang.Nullable;
+import lombok.Data;
 
+@Data
 public class UserBO {
 
-	@Size(max = 1024)
-	@NotNull
-	@NotBlank
+	private String Id;
+	
 	private String firstName;
 	
-	@Size(max = 1024)
-	@NotNull
-	@NotBlank
 	private String lastName;
 	
-	@Nullable
 	private String email;
+	
+	private String phone;					
+	
+	private String houseNumber;
+	
+	private String street;
+	
+	private String city;
+	
+	private String postCode;
+	
+	private String state;
+	
+	private String country;
+	
+	private String agentId;	
+	
+	private boolean isDeleted;
+	
+	public void UserBo(User user)
+	{
+		this.Id=user.getId();
+		this.firstName=user.getFirstName();
+		this.lastName=user.getLastName();
+		this.email=user.getEmail();
+		this.phone=user.getPhone();		
+		this.houseNumber=user.getHouseNumber();
+		this.street=user.getStreet();
+		this.city=user.getCity();
+		this.postCode=user.getPostCode();
+		this.state=user.getState();
+		this.country=user.getCountry();
+		this.agentId=user.getAgentId();						
+	}
+	
+	
 }
--- src/main/java/com/toqqa/domain/User.java
+++ src/main/java/com/toqqa/domain/User.java
@@ -1,10 +1,67 @@
 package com.toqqa.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name="user_info")
 public class User {
-
+	
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")	
+	private String id;
+	
+	@Size(max = 1024)
+	@NotNull
+	@NotBlank
+	private String firstName;
+	
+	@Size(max = 1024)
+	@NotNull
+	@NotBlank
+	private String lastName;
+	
+	@Column(unique = true,nullable = true)
+	private String email;
+	
+	@NotNull
+	@Column(unique = true)	
+	private String phone;				
+	
+	private String houseNumber;
+	
+	private String street;
+	
+	private String city;
+	
+	private String postCode;
+	
+	private String state;
+	
+	private String country;
+		
+	private String agentId;
+	
+	private boolean isDeleted;
+	
+	@ManyToMany(mappedBy = "users")
+	private final List<UserType> userTypes = new ArrayList<>();		
+		
 }
+
