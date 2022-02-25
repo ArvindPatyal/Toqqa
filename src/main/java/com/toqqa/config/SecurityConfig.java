feb8ad425ae5e7db7e2a29f0748c9b638f12f120
'Rajat <rajat.sharma@biztecno.net>
2022-02-25 19:08:44 +0530

    Removed unwanted endpoint configuration from security config

--- src/main/java/com/toqqa/config/SecurityConfig.java
+++ src/main/java/com/toqqa/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(entryPoint).and()
 				.authorizeRequests()
 				.antMatchers("/api/auth/**", "/swagger-ui/**", "/configuration/**", "/swagger-resources/**",
-						"/v2/api-docs", "/webjars/**", "/api/business/**", "/api/registration/**","/file/upload/**","/api/sme/fetchSme/**")
+						"/v2/api-docs", "/webjars/**", "/api/business/**", "/api/registration/**","/file/upload/**")
 				.permitAll().anyRequest().authenticated().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
