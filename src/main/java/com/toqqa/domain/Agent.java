e380de9e94557c6044a37f24e47e4e9d082bb9d1
Arvind <arvind.patyal@biztecno.net>
2022-06-17 12:34:42 +0530

    agentId added in agent api

--- src/main/java/com/toqqa/domain/Agent.java
+++ src/main/java/com/toqqa/domain/Agent.java
@@ -1,16 +1,18 @@
 package com.toqqa.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -21,17 +23,20 @@ import javax.validation.constraints.NotNull;
 @Table
 public class Agent {
 
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
-    private String id;
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+	private String id;
 
-    @NotNull
-    private String agentDocuments;
+	@NotNull
+	private String agentDocuments;
 
-    @NotNull
-    private String idProof;
+	@NotNull
+	private String idProof;
+	
+	private String userId;
 
-    private String userId;
+	@Column(unique = true)
+	private String agentId;
 
 }
--- src/main/java/com/toqqa/util/Constants.java
+++ src/main/java/com/toqqa/util/Constants.java
@@ -13,18 +13,20 @@ public class Constants {
 	public static final String ERR_NO_CURRENT_ADDRESS = "No address has beeen set as a current address.";
 
 	public static final String ERR_USER_NOT_SME = "Current user is not having role SME.";
-    
-    public static final String TOTAL_ORDER_AMOUNT_DELIVERED_QUERY_BY_DATE ="select sum(o.amount) from order_info o where o.sme_id =?1"
-    		+ " and o.order_status='DELIVERED' and cast(created_date as DATE) between ?2 AND ?3";
-    
-    public static final String ORDER_COUNT_QUERY_BY_DATE_AND_STATUS = "select count(o.id) from order_info o where o.sme_id =?1 and "
-    		+ "o.order_status=?2 and cast(created_date as DATE) between ?3 AND ?4";
-    
-    public static final String TOTAL_ORDER_ITEM_COUNT_QUERY_BY_DATE = "select sum(i.quantity) from order_item i inner join order_info o on i.order_id = o.id where o.sme_id =?1 and o.order_status='DELIVERED'\n" + 
-    		"and cast(created_date as DATE) between ?2 AND ?3";
-    
+
+	public static final String TOTAL_ORDER_AMOUNT_DELIVERED_QUERY_BY_DATE = "select sum(o.amount) from order_info o where o.sme_id =?1"
+			+ " and o.order_status='DELIVERED' and cast(created_date as DATE) between ?2 AND ?3";
+
+	public static final String ORDER_COUNT_QUERY_BY_DATE_AND_STATUS = "select count(o.id) from order_info o where o.sme_id =?1 and "
+			+ "o.order_status=?2 and cast(created_date as DATE) between ?3 AND ?4";
+
+	public static final String TOTAL_ORDER_ITEM_COUNT_QUERY_BY_DATE = "select sum(i.quantity) from order_item i inner join order_info o on i.order_id = o.id where o.sme_id =?1 and o.order_status='DELIVERED'\n"
+			+ "and cast(created_date as DATE) between ?2 AND ?3";
+
 	public static final String MSG_NO_BUSINESS_LOGO = "There is no business logo found for sme.";
 
 	public static final String ORDER_CONSTANT = "ORD-";
+
+	public static final String AGENT_CONSTANT = "AGT-";
 
 }
