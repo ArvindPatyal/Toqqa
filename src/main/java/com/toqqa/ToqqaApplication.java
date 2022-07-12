e3e9408c184d646934bafa8a75e157a569f525d6
vishwajeetsingh125 <99714038+vishwajeetsingh125@users.noreply.github.com>
2022-07-12 12:18:13 +0530

    changed Time Zone to PLT

--- src/main/java/com/toqqa/ToqqaApplication.java
+++ src/main/java/com/toqqa/ToqqaApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
 public class ToqqaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToqqaApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("PLT"));
 	}
 }
