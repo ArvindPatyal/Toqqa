247b90712ab4f31a47e9e9c9693be99be66d919d
Arvind <arvindrintu05@gmail.com>
2022-07-26 22:19:42 +0530

    Admin login dto

--- src/main/java/com/toqqa/payload/LoginRequestAdmin.java
+++ src/main/java/com/toqqa/payload/LoginRequestAdmin.java
@@ -1,24 +1,22 @@
 package com.toqqa.payload;
 
-import javax.validation.constraints.NotBlank;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
-	@NotBlank
-	private String username;
+public class LoginRequestAdmin {
 
-	@NotBlank
-	private String password;
+    @NotBlank
+    private String username;
 
-	@NotBlank
-	private String deviceToken;
+    @NotBlank
+    private String password;
 
 }
